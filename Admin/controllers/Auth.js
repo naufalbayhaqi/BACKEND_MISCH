24ae89f3f3c3fd436353228d3a29eb31e02fd2ae
unknown <titan170300@gmail.com>
2022-01-31 23:24:32 +0700

    TES

--- Admin/controllers/Auth.js
+++ Admin/controllers/Auth.js
@@ -3,113 +3,114 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const getUsers = async (req, res) => {
-	try {
-		const users = await Users.findAll({
-			attributes: ["id", "name", "email"],
-		});
-		res.json(users);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const users = await Users.findAll({
+      attributes: ["id", "name", "email"],
+    });
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const Login = async (req, res) => {
-	try {
-		const user = await Users.findOne({
-			where: {
-				username: req.body.username,
-			},
-			attributes: { exclude: ["createdAt", "updatedAt"] },
-		});
-		// console.log(user);
-		const match = await bcrypt.compare(req.body.password, user.password);
-		if (!match) return res.status(400).json({ msg: "Password salah" });
-		const userId = user.id;
-		const name = user.name;
-		const username = user.username;
-		const tenantId = user.tenantId;
-		const role = user.role;
-		const accessToken = jwt.sign(
-			{ userId, username, tenantId, role },
-			"KONTOL",
-			{
-				expiresIn: "20s",
-			}
-		);
-		const refreshToken = jwt.sign(
-			{ userId, username, tenantId, role },
-			"MEMEK",
-			{
-				expiresIn: "1d",
-			}
-		);
-		await Users.update(
-			{ refresh_token: refreshToken },
-			{
-				where: {
-					id: userId,
-				},
-			}
-		);
-		res.cookie("refreshToken", refreshToken, {
-			httpOnly: true,
-			maxAge: 24 * 60 * 60 * 1000,
-			secure: false,
-		});
-		res.json({ accessToken, userId, username, tenantId, name });
-	} catch (error) {
-		console.log(error);
-		res.status(404).json({ msg: "Username tidak ditemukan" });
-	}
+  try {
+    const user = await Users.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    // console.log(user);
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      const userId = user.id;
+      const name = user.name;
+      const username = user.username;
+      const tenantId = user.tenantId;
+      const role = user.role;
+      const accessToken = jwt.sign(
+        { userId, username, tenantId, role },
+        "KONTOL",
+        {
+          expiresIn: "20s",
+        }
+      );
+      const refreshToken = jwt.sign(
+        { userId, username, tenantId, role },
+        "MEMEK",
+        {
+          expiresIn: "1d",
+        }
+      );
+      await Users.update(
+        { refresh_token: refreshToken },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false,
+      });
+      res.status(200).send({ accessToken, userId, username, tenantId, name });
+    } else {
+      res.status(400).send(err);
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 
 export const Register = async (req, res) => {
-	if (req.body.password !== req.body.konfirmasi)
-		return res.status(400).json({ msg: "Password tidak cocok" });
-	const salt = await bcrypt.genSalt();
-	const hashPassword = await bcrypt.hash(req.body.password, salt);
-	try {
-		await Users.create({
-			name: req.body.name,
-			username: req.body.username,
-			password: hashPassword,
-			tenantId: req.body.tenantId,
-			role: req.body.role,
-		});
-		res.json({ msg: "Register Berhasil" });
-	} catch (error) {
-		// console.log(error);
-		res.json(error.message);
-	}
+  try {
+    if (req.body.password !== req.body.konfirmasi)
+      return res.status(400).json({ msg: "Password tidak cocok" });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    await Users.create({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashPassword,
+      tenantId: req.body.tenantId,
+      role: req.body.role,
+    });
+    res.json({ msg: "Register Berhasil" });
+  } catch (error) {
+    // console.log(error);
+    res.json(error.message);
+  }
 };
 
 export const Logout = async (req, res) => {
-	try {
-		const refreshToken = req.cookies["refreshToken"];
-		if (!refreshToken) return res.sendStatus(204);
-		const user = await Users.findAll({
-			where: {
-				refresh_token: refreshToken,
-			},
-		});
-		console.log(user[0]);
-		if (!user[0]) {
-			return res.sendStatus(204);
-		} else {
-			const userId = user[0].id;
-			// console.log(userId);
-			await Users.update(
-				{ refresh_token: null },
-				{
-					where: {
-						id: userId,
-					},
-				}
-			);
-			res.clearCookie("refreshToken");
-			return res.sendStatus(200);
-		}
-	} catch (err) {
-		res.status(400);
-	}
+  try {
+    const refreshToken = req.cookies["refreshToken"];
+    if (!refreshToken) return res.sendStatus(204);
+    const user = await Users.findAll({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+    console.log(user[0]);
+    if (!user[0]) {
+      return res.sendStatus(204);
+    } else {
+      const userId = user[0].id;
+      // console.log(userId);
+      await Users.update(
+        { refresh_token: null },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+      res.clearCookie("refreshToken");
+      return res.sendStatus(200);
+    }
+  } catch (err) {
+    res.status(400);
+  }
 };
--- Admin/middleware/VerifyToken.js
+++ Admin/middleware/VerifyToken.js
@@ -1,15 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-	const authHeader = req.headers["authorization"];
-	const token = authHeader && authHeader.split(" ")[1];
-	if (token == null) return res.sendStatus(401);
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err) return res.sendStatus(403);
-		req.username = decoded.username;
-		req.tenantId = decoded.tenantId;
-		req.role = decoded.role;
-		req.userId = decoded.userId;
-		next();
-	});
+  try {
+    const authHeader = req.cookies["refreshToken"];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.status(401).send("Invalid Token");
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) return res.sendStatus(403);
+      req.username = decoded.username;
+      req.tenantId = decoded.tenantId;
+      req.role = decoded.role;
+      req.userId = decoded.userId;
+      next();
+    });
+  } catch (err) {
+    res.status(401).send("Invalid Token");
+  }
 };
